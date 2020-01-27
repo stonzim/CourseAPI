af5711e9d05d628fafe7ac713a82f6593b454c01
Winston Downes <winston_downes@hotmail.com>
2020-01-27 22:07:09 +1100

    working

--- springboot-starter/src/main/java/main/CourseApiApp.java
+++ springboot-starter/src/main/java/main/CourseApiApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CourseApiApp extends SpringBootServletInitializer{
+public class CourseApiApp{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApiApp.class, args);     
