c71b7c813d9cd77ace7967058b198a936bdb500d
Winston Downes <winston_downes@hotmail.com>
2020-01-30 04:18:30 +1100

    Changed controller annotations and added start-class to pom

--- springboot-starter/src/main/java/main/course/CourseController.java
+++ springboot-starter/src/main/java/main/course/CourseController.java
@@ -3,7 +3,10 @@ package main.course;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,19 +30,22 @@ public class CourseController {
 		return courseService.getCourse(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="/topics/{topicId}/courses/")
+//	@RequestMapping(method=RequestMethod.POST, value="/topics/{topicId}/courses/")
+	@PostMapping("/topics/{topicId}/courses/")
 	public void addCourse(@RequestBody Course course, @PathVariable int topicId) {
 		course.setTopic(new Topic(topicId, "", ""));
 		courseService.addCourse(course);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, value="/topics/{topicId}/courses/{id}")
+//	@RequestMapping(method=RequestMethod.PUT, value="/topics/{topicId}/courses/{id}")
+	@PutMapping("/topics/{topicId}/courses/{id}")
 	public void updateCourse(@RequestBody Course course, @PathVariable int topicId, @PathVariable int id) {
 		course.setTopic(new Topic(topicId, "", ""));
 		courseService.updateCourse(course);
 	}
 	
-	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{topicId}/courses/{id}")
+//	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{topicId}/courses/{id}")
+	@DeleteMapping("/topics/{topicId}/courses/{id}")
 	public void deleteCourse(@PathVariable int id) {
 		courseService.deleteCourse(id);
 	}
--- springboot-starter/src/main/java/main/topic/TopicController.java
+++ springboot-starter/src/main/java/main/topic/TopicController.java
@@ -3,7 +3,10 @@ package main.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,17 +28,20 @@ public class TopicController {
 		return topicService.getTopic(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value="/topics")
+//	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	@PostMapping("/topics")
 	public void addTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
 	}
 	
-	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+//	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	@PutMapping("/topics/{id}")
 	public void updateTopic(@RequestBody Topic topic, @PathVariable int id) {
 		topicService.updateTopic(topic, id);
 	}
 	
-	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+//	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	@DeleteMapping("/topics/{id}")
 	public void deleteTopic(@PathVariable int id) {
 		topicService.deleteTopic(id);
 	}
