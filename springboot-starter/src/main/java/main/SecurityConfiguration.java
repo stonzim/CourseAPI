62c7fa18c8bacccded1b8bf5bf3fb0c496322e73
Winston Downes <winston_downes@hotmail.com>
2020-04-23 17:57:52 +1000

    added admin page

--- springboot-starter/src/main/java/main/SecurityConfiguration.java
+++ springboot-starter/src/main/java/main/SecurityConfiguration.java
@@ -31,6 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+//		.antMatchers("/admin").hasRole("admin")
 		.antMatchers("/topics").hasRole("user")
 		.antMatchers("/").permitAll()
 		.and().formLogin();
---
+++ springboot-starter/src/main/java/main/admin/AdminController.java
@@ -0,0 +1,12 @@
+package main.admin;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+public class AdminController {
+	@RequestMapping("/admin")
+	public String welcome() {
+		return "Admin Page";
+	}
+}
